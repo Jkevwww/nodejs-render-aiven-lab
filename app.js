bbe69bf58be3fc4a1c189424107c9ae997619362
Jerome Kevin Fiel <2310030-1@southernleytestateu.edu.ph>
2026-04-14 11:37:58 +0800

    commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: true }
+  ssl: { rejectUnauthorized: false }
 });
 
 app.get("/", (req, res) => {
